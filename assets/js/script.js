92a88b6c32ddf35f8b723276935a2fbc69bc5e7a
Carolyn B <bmathematics@gmail.com>
2022-01-21 18:01:17 -0500

    logic to display question

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,7 +12,7 @@ var incorrectEl = document.querySelector("#incorrect");
 var scoreEl = document.querySelector("#score");
 var resultEl = document.querySelector("#result");
 var highscoresEl = document.querySelector(".highscores");
-
+var timerEl = document.getElementById('timer');
 var currentQuestion = 0;
 
 var secondsLeft = 20;
@@ -34,109 +34,155 @@ var bank = [
         "B.  The Web server",
         "C.  A central machine deep within Netscape's corporate offices",
         "D.  None of the above",
-    ]
-            
-        ,
+    ],
     correctAnswer: "a"
     },
+
     {question: "What are variables used for in JavaScript Programs?",
-    answers:{
-            a:"A.  Storing numbers, dates, or other values",
-            b:"B.  Varying randomly",
-            c:"C.  Causing high-school algebra flashbacks",
-            d:"D.  None of the above",
-        },
+    answers:[
+        "A.  Storing numbers, dates, or other values",
+        "B.  Varying randomly",
+        "C.  Causing high-school algebra flashbacks",
+        "D.  None of the above",
+    ],
     correctAnswer: "a"
     },
+
     {question: "_____ JavaScript statements embedded in an HTML page can respond to user events such as mouse-clicks, form input, and page navigation.",
-    answers:{
-            a:"A.  Client-side",
-            b:"B.  Server-side",
-            c:"C.  Local",
-            d:"D.  Native",
-        },
+    answers:[
+        "A.  Client-side",
+        "B.  Server-side",
+        "C.  Local",
+        "D.  Native",
+    ],
     correctAnswer: "a"
     },
+
     {question: "Which of the following are capabilities of functions in JavaScript?",
-    answers:{
-            a:"A.  Return a value",
-            b:"B.  Accept parameters and Return a value",
-            c:"C.  Accept parameters",
-            d:"D.  None of the above",
-        },
+    answers:[
+            "A.  Return a value",
+            "B.  Accept parameters and Return a value",
+            "C.  Accept parameters",
+            "D.  None of the above",
+    ],
     correctAnswer: "c"
     },
+
     {question: "Which of the following is not a valid JavaScript variable name?",
-    answers:{
-            a:"A.  _first_and_last_names",
-            b:"B.  2names",
-            c:"C.  FirstAndLast",
-            d:"D.  None of the above",
-        },
+    answers:[
+        "A.  _first_and_last_names",
+            "B.  2names",
+            "C.  FirstAndLast",
+            "D.  None of the above",
+    ],
     correctAnswer: "b"
     },
+
     {question: "What is the correct syntax for referring to an external script called ' abc.js'?",
-    answers:{
-            a:"A. <script href=' abc.js'>",
-            b:"B. <script name=' abc.js'>",
-            c:"C. <script src=' abc.js'>",
-            d:"D. None of the above",
-        },
+    answers:[
+        "A. <script href=' abc.js'>",
+            "B. <script name=' abc.js'>",
+            "C. <script src=' abc.js'>",
+            "D. None of the above",
+    ],
     correctAnswer: "c"
     },
+
     {question: "Which types of image maps can be used with JavaScript?",
-    answers:{
-            a:"A.  Server-side image maps",
-            b:"B.  Client-side image maps",
-            c:"C.  Server-side image maps and Client-side image maps",
-            d:"D.  None of the above",
-        },
+    answers:[
+            "A.  Server-side image maps",
+            "B.  Client-side image maps",
+            "C.  Server-side image maps and Client-side image maps",
+            "D.  None of the above",
+    ],
     correctAnswer: "b"
     },
+
     {question: "Which is the correct way to write a JavaScript array?",
-    answers:{
-            a:"A.  var txt = new Array(1:'tim',2:'kim',3:'jim')",
-            b:"B.  var txt = new Array:1=('tim')2=('kim')3=('jim')",
-            c:"C.  var txt = new Array('tim','kim','jim')",
-            d:"D.  var txt = new Array='tim','kim','jim'",
-        },
+    answers:[
+            "A.  var txt = new Array(1:'tim',2:'kim',3:'jim')",
+            "B.  var txt = new Array:1=('tim')2=('kim')3=('jim')",
+            "C.  var txt = new Array('tim','kim','jim')",
+            "D.  var txt = new Array='tim','kim','jim'",
+    ],
     correctAnswer: "c"
     },
+
     {question: "JavaScript entities start with _______ and end with _________.",
-    answers:{
-            a:"A.  Semicolon, colon",
-            b:"B.  Semicolon, Ampersand",
-            c:"C.  Ampersand, colon",
-            d:"D.  Ampersand, semicolon",
-        },
+    answers:[
+            "A.  Semicolon, colon",
+            "B.  Semicolon, Ampersand",
+            "C.  Ampersand, colon",
+            "D.  Ampersand, semicolon",
+    ],
     correctAnswer: "d"
     },
+
     {question: "Which of the following is the structure of an if statement?",
-    answers:{
-            a:"A.  if (conditional expression is true) thenexecute this codeend if",
-            b:"B.  if (conditional expression is true)execute this codeend if",
-            c:"C.  if (conditional expression is true)   {then execute this code>->}",
-            d:"D.  if (conditional expression is true) then {execute this code}",
-        },
+    answers:[
+            "A.  if (conditional expression is true) thenexecute this codeend if",
+            "B.  if (conditional expression is true)execute this codeend if",
+            "C.  if (conditional expression is true)   {then execute this code>->}",
+            "D.  if (conditional expression is true) then {execute this code}",
+    ],
     correctAnswer: "c"
     },
+
     {question: "The _______ method of an Array object adds and/or removes elements from an array.",
-    answers:{
-            a:"A.  Reverse",
-            b:"B.  Shift",
-            c:"C.  Slice",
-            d:"D.  Splice",
-        },
+    answers:[
+            "A.  Reverse",
+            "B.  Shift",
+            "C.  Slice",
+            "D.  Splice",
+    ],
     correctAnswer: "d"
     },
 ]
+//set timer on page
+var countdown = function(){
+    secondsLeft = 20;
+    //count down on timer
+    var timer = setInterval(function(){
+    timerEl.innerHTML = secondsLeft;
+    
+    
+        if(secondsLeft > -1){
+            
+            secondsLeft --;
+        }else{
+            //clear the interval
+            clearInterval(timer);
+            alert("Your time is up! Let's see your score");
+            timerEl.innerHTML = 0;
+        }
+            
+    }, 1000);
+};
+//filter through and display question answer choices
+// var getChoices = function(){
+//     for(i = 0; i < 4; i++){
+//         choiceA.textContent = 
+//     }
+// }
+//show questions for the quiz
+var pullQuestions = function(){
+    for (index = 0; index < bank.length; index++){
+        questionsEl.innerHTML = "<p>" + bank[index].question + "<p/>";
+        
+        
+    }
+}
+
+
 
 //user clicks start
 startEl.addEventListener("click", function(){
     //timer starts counting down
+    startEl.setAttribute("data-stat", "hidden");
+    countdown();
     
     //first question appears
-
+    pullQuestions();
     //capture the event (item chosen)
 
     //check the event to the correct Answer
